c08e012db93e7c3b251d013a46d0c9c2cc9bb2a0
Camilo Castro <camilo@cervezapps.cl>
2014-10-04 17:32:57 -0300

    Created constructor for #3

--- webpaykcc/webpaykcc.php
+++ webpaykcc/webpaykcc.php
@@ -3,6 +3,51 @@
 if (!defined('_PS_VERSION_'))
     exit;
 
+/**
+* This Class Holds the Admin
+* Part of the Webpay Module
+*/
 class WebpayKcc extends PaymentModule {
 
+	public function __construct() {
+		
+		// Basic Settings
+
+		$this->name = 'webpaykcc';
+		$this->tab = 'payments_gateway';
+
+		$this->version = '1.0.0';
+		$this->author = 'Camilo Castro <camilo@cervezapps.cl>';
+
+		$this->need_instance = 1;
+
+		$this->ps_versions_compliancy = array(
+								'min' => '1.6', 
+								'max' => __PS_VERSION__);
+
+		// This is for the views
+		// This module does not use twitter bootstrap
+		// helpers. But someday in the future will.
+		// meanwhile is turned off.
+		// $this->bootstrap = true;
+		// $this->bootstrap = false;
+
+		// Call the Parent Constructor
+		parent::__construct();
+
+		// Now some messages
+		$this->displayName = $this->l('Webpay KCC Payment');
+
+		$this->description = $this->l("Payment Gateway using Chile's Transbank Webpay KCC");
+
+		$this->confirmUninstall = $this->l("Payments with Webpay KCC will not be possible. Are you sure to uninstall?");
+
+
+		// Call internal setup methods
+		$this->setModuleSettings();
+
+		$this->checkModuleRequirements();
+
+
+	}
 }
