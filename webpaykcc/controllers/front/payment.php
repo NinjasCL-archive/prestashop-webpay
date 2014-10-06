1189745e5bc34ee7386b69f81a03b8a5e71950f0
Camilo Castro <camilo@cervezapps.cl>
2014-10-05 22:19:28 -0300

    Added Base structure for #11

--- webpaykcc/controllers/front/payment.php
+++ webpaykcc/controllers/front/payment.php
@@ -63,7 +63,7 @@ class WebpayKccPaymentModuleFrontController
 	  		
 	  		// Instance a new variable
 	  		// and call the parent init method
-	  		$webpay = new WebpayKcc();
+	  		$webpaykcc = new WebpayKcc();
 	  		
 	  		parent::initContent();
 
@@ -75,8 +75,67 @@ class WebpayKccPaymentModuleFrontController
 	  		$customer = $this->context->customer;
 
 	  		// Get KCC Vars
-	  		$kccPath = Configuration::get()
+	  		$kccPath = Configuration::get(KCC_PATH);
+	  		
+	  		$kccURL = Configuration::get(KCC_URL);
 
+	  		$kccLogPath = Configuration::get(KCC_LOG);
+
+	  		// Round total amount
+	  		// of user cart
+			$total_amount = Tools:ps_round(
+							floatval(
+								$cart->getOrderTotal(
+									true, 
+									Cart::BOTH)
+								) 0);
+
+			// Base URL
+			// for generating pages
+			$base_url = Tools::getShopDomainSsl(true, true) 
+						. __PS_BASE_URI__;
+
+ 			// Base URL for success
+			// or failure pages
+			$module_url = . "index.php?fc=module&module="
+						. "{$webpaykcc->name}&controller="
+						. "validate&cartId=" 
+						. $cartId;
+
+
+			// Transbank will
+			// call this page when
+			// the transaction is successful
+			$success_page = $base_url . $module_url . "&return=ok";
+
+			// Transbank will
+			// call this page when
+			// the transaction is not right
+			$failure_page = $base_url . $module_url . "&return=error";
+
+			// This page will
+			// be called when the transaction
+			// is being made.
+			// this page will update the cart
+			// data, register the logs and
+			// tell transbank if all is correct.
+			// then transbank will call success
+			// or failure depending on the
+			// result of this page
+			$callback_page = $base_url 	
+						. "modules/{$webpaykcc->name}"
+						. "/validate.php";
+
+
+			// Now we pass the data
+			// to smarty and render
+			// the template
+
+			$this->context->smarty->assign(array(
+
+			));
+
+			$this->setTemplate('confirmation.tpl');	
 
 	  	}
 
