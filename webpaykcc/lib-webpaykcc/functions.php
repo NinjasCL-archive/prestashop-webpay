f4190aa54b4fd0df8d99f6ea90278ab1a659695d
Camilo Castro <camilo@cervezapps.cl>
2014-10-05 22:58:23 -0300

    Added log path helper function for #11

--- webpaykcc/lib-webpaykcc/functions.php
+++ webpaykcc/lib-webpaykcc/functions.php
@@ -1,3 +1,17 @@
 <?php
 
+include_once './constants.php';
+
 // Webpay Related Functions and Helpers
+
+// Get the log path for a given
+// path and session id
+function getKCCLog($path, $session_Id) {
+	
+	$logPath = $path 
+			   . KCC_LOG_PREFIX
+		  	   . $session_id
+			   . 'log';
+
+	return $logPath;
+}
