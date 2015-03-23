c605b3c8f73aff19045833d30f2ff422ec40460d
Camilo Castro <camilo@jumpitt.com>
2015-03-23 09:58:56 -0300

    added translations close #19

---
+++ webpaykcc/translations/es.php
@@ -0,0 +1,23 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{webpaykcc}prestashop>webpaykcc_a62cf85ce6655e24044ed72412f3b936'] = 'Pago con Webpay KCC';
+$_MODULE['<{webpaykcc}prestashop>webpaykcc_ab7a9849c7fc2e8fdd99dab243ca6efd'] = 'Falta definir el Directorio del KCC';
+$_MODULE['<{webpaykcc}prestashop>webpaykcc_9eef733d37728f4a77d2b1fa7ba727e8'] = 'Falta definir la URL del KCC';
+$_MODULE['<{webpaykcc}prestashop>webpaykcc_a9912f1f5ba8f2453c473e4fb08cc2d0'] = 'Falta definir el Directorio Log del KCC';
+$_MODULE['<{webpaykcc}prestashop>webpaykcc_a4488ebfcc076240fdf97548fc251a32'] = 'Falta definir la URL de la Página de Términos y Condiciones';
+$_MODULE['<{webpaykcc}prestashop>config_6c7057a8937c5d23f94d7bc06d4b60c7'] = 'Pago usando Webpay';
+$_MODULE['<{webpaykcc}prestashop>config_a82be0f551b8708bc08eb33cd9ded0cf'] = 'Información';
+$_MODULE['<{webpaykcc}prestashop>config_b1c1d84a65180d5912b2dee38a48d6b5'] = 'Versión ';
+$_MODULE['<{webpaykcc}prestashop>config_76bf3f50263e1c327511858d64f199ef'] = 'Ruta Base';
+$_MODULE['<{webpaykcc}prestashop>config_61fbbed355926a3e60702b610515ee3d'] = 'Página de Validación';
+$_MODULE['<{webpaykcc}prestashop>config_f4f70727dc34561dfde1a3c529b6205c'] = 'Ajustes';
+$_MODULE['<{webpaykcc}prestashop>config_d638a83460aa5681418e1d7fde016045'] = 'Directorio del KCC';
+$_MODULE['<{webpaykcc}prestashop>config_c6b4b53800ee5e86e8daefbb41861b4a'] = 'URL del KCC';
+$_MODULE['<{webpaykcc}prestashop>config_583b192b7e5290c858ba26e804c4ecb8'] = 'Directorio de Log KCC';
+$_MODULE['<{webpaykcc}prestashop>config_856ca47962acab608851d7812da96241'] = 'Página de Términos y Condiciones';
+$_MODULE['<{webpaykcc}prestashop>config_9daf1fb753b42c3cdc8f1d01669cd6d8'] = 'Guardar Cambios';
+$_MODULE['<{webpaykcc}prestashop>confirmation_660d604f24f1cee6730e33262cc98857'] = 'Pagar Usando Webpay';
+$_MODULE['<{webpaykcc}prestashop>confirmation_8630898aa8a2c31f18ff406bcab447fd'] = 'El monto total de la compra es el siguiente';
+$_MODULE['<{webpaykcc}prestashop>confirmation_3dacb130bac464075f958f72f1148886'] = 'Pagar Carro';
